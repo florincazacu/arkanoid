54f5828ec32993f1e6bd27e3bc7bb04b8cc36cf6
florin <florin.ics@gmail.com>
2018-01-12 16:54:25 +0200

    Added Ball reflect direction

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -20,4 +20,36 @@ public class Ball : MonoBehaviour {
         GetComponent<Rigidbody2D>().transform.position = temp;
         GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
     }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        // This function is called whenever the ball
+        // collides with something
+
+        if (col.gameObject.name == "racket") {
+            // Calculate hit Factor
+            float x = hitFactor(transform.position,
+                              col.transform.position,
+                              col.collider.bounds.size.x);
+
+            // Calculate direction, set length to 1
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        if (col.gameObject.name == "border_bottom") {
+            GameManager.instance.SendMessage("ballOut");
+        }
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+            float racketWidth) {
+        // ascii art:
+        //
+        // 1  -0.5  0  0.5   1  <- x value
+        // ===================  <- racket
+        //
+        return (ballPos.x - racketPos.x) / racketWidth;
+    }
 }
